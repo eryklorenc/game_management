e04035cc52c4e5b29cfe537ad91c806a7684405f
Eryk Lorenc <reus2000@wp.pl>
2023-03-07 03:22:34 +0100

    Put items collection into separate users

--- lib/repositories/items_repository_library.dart
+++ lib/repositories/items_repository_library.dart
@@ -1,10 +1,17 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:game_management/features/home/library_page/library_page_content.dart';
 import 'package:game_management/models/item_model_library.dart';
 
 class ItemsRepositoryLibrary {
   Stream<List<ItemModelLibrary>> getItemsStream() {
+    final userID = FirebaseAuth.instance.currentUser?.uid;
+    if (userID == null) {
+      throw Exception('User is not logged in');
+    }
     return FirebaseFirestore.instance
+        .collection('users')
+        .doc(userID)
         .collection('games')
         .snapshots()
         .map((querySnapshot) {
@@ -21,7 +28,15 @@ class ItemsRepositoryLibrary {
   }
 
   Future<void> add() async {
-    await FirebaseFirestore.instance.collection('games').add(
+    final userID = FirebaseAuth.instance.currentUser?.uid;
+    if (userID == null) {
+      throw Exception('User is not logged in');
+    }
+    await FirebaseFirestore.instance
+        .collection('users')
+        .doc(userID)
+        .collection('games')
+        .add(
       {
         'title': controller.text,
         'status': controllerstatus.text,
@@ -30,6 +45,15 @@ class ItemsRepositoryLibrary {
   }
 
   Future<void> delete({required String id}) {
-    return FirebaseFirestore.instance.collection('games').doc(id).delete();
+    final userID = FirebaseAuth.instance.currentUser?.uid;
+    if (userID == null) {
+      throw Exception('User is not logged in');
+    }
+    return FirebaseFirestore.instance
+        .collection('users')
+        .doc(userID)
+        .collection('games')
+        .doc(id)
+        .delete();
   }
 }
--- lib/repositories/items_repository_wish_list.dart
+++ lib/repositories/items_repository_wish_list.dart
@@ -1,10 +1,17 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:game_management/features/home/wish_list/wish_list_page_content.dart';
 import 'package:game_management/models/item_model_wish_list.dart';
 
 class ItemsRepositoryWishList {
   Stream<List<ItemModelWishList>> getItemsStream() {
+    final userID = FirebaseAuth.instance.currentUser?.uid;
+    if (userID == null) {
+      throw Exception('User is not logged in');
+    }
     return FirebaseFirestore.instance
+        .collection('users')
+        .doc(userID)
         .collection('wishes')
         .snapshots()
         .map((querySnapshot) {
@@ -20,7 +27,15 @@ class ItemsRepositoryWishList {
   }
 
   Future<void> add() async {
-    await FirebaseFirestore.instance.collection('wishes').add(
+    final userID = FirebaseAuth.instance.currentUser?.uid;
+    if (userID == null) {
+      throw Exception('User is not logged in');
+    }
+    await FirebaseFirestore.instance
+        .collection('users')
+        .doc(userID)
+        .collection('wishes')
+        .add(
       {
         'title': controller1.text,
       },
@@ -28,6 +43,15 @@ class ItemsRepositoryWishList {
   }
 
   Future<void> delete({required String id}) {
-    return FirebaseFirestore.instance.collection('wishes').doc(id).delete();
+    final userID = FirebaseAuth.instance.currentUser?.uid;
+    if (userID == null) {
+      throw Exception('User is not logged in');
+    }
+    return FirebaseFirestore.instance
+        .collection('users')
+        .doc(userID)
+        .collection('wishes')
+        .doc(id)
+        .delete();
   }
 }
