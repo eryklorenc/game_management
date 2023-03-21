9cf2828a91c1c1accf3ad16694426029e91b84a6
Eryk Lorenc <reus2000@wp.pl>
2023-03-21 22:33:13 +0100

    Add .fromJson method to our model

--- lib/models/item_model_most_popular.dart
+++ lib/models/item_model_most_popular.dart
@@ -3,4 +3,7 @@ class ItemModelMostPopular {
     required this.content,
   });
   final String content;
+
+  ItemModelMostPopular.fromJson(Map<String, dynamic> json)
+      : content = json['body'];
 }
--- lib/repositories/items_repository_most_popular.dart
+++ lib/repositories/items_repository_most_popular.dart
@@ -1,4 +1,3 @@
-
 import 'package:game_management/data/remote_data_sources/most_popular_remote_data_sources.dart';
 import 'package:game_management/models/item_model_most_popular.dart';
 
@@ -7,16 +6,14 @@ class ItemsRepositoryMostPopular {
   final MostPopularRemoteDataSource _mostPopularRemoteDataSource;
 
   Future<ItemModelMostPopular?> getItemModelMostPopular() async {
-    final responseData = await _mostPopularRemoteDataSource.getDataMostPopular();
+    final json =
+        await _mostPopularRemoteDataSource.getDataMostPopular();
 
-    if (responseData == null) {
+    if (json == null) {
       return null;
     }
 
-    final content = responseData['body'] as String;
+    return ItemModelMostPopular.fromJson(json);
 
-    return ItemModelMostPopular(
-      content: content,
-    );
   }
 }
