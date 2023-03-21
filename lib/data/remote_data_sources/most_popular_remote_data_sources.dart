55fe9e5b4ba38fc112632adab8030b61001efb27
Eryk Lorenc <reus2000@wp.pl>
2023-03-21 23:20:49 +0100

    Add dio handling errors

--- lib/data/remote_data_sources/most_popular_remote_data_sources.dart
+++ lib/data/remote_data_sources/most_popular_remote_data_sources.dart
@@ -1,10 +1,14 @@
 import 'package:dio/dio.dart';
 
 class MostPopularRemoteDataSource {
-  Future<Map<String,dynamic>?> getDataMostPopular() async {
-    final response = await Dio().get<Map<String, dynamic>>(
-        'https://jsonplaceholder.typicode.com/posts/1');
-    return response.data;
-
+  Future<Map<String, dynamic>?> getDataMostPopular() async {
+    try {
+      final response = await Dio().get<Map<String, dynamic>>(
+          'https://jsonplaceholder.typicode.com/posts/1');
+      return response.data;
+    } on DioError catch (error) {
+      throw Exception(
+          error.response?.data['error']['message'] ?? 'Unknown error');
+    }
   }
 }
